dc84a76922eab636df92546c2bebb63bd7d73a88
Luke Opperman <luke@opperman.net>
2016-09-08 14:41:36 -0400

    Move form inputs to render() sub-templates

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {NxusModule} from 'nxus-core'
+import {templater} from 'nxus-templater'
 import MVCModule from './MVCModule'
 import ViewController from './ViewController'
 import EditController from './EditController'
@@ -6,6 +7,10 @@ import EditController from './EditController'
 class Web extends NxusModule {
   constructor() {
     super()
+
+    // Expose base controller templates at global names
+    templater.default().templateDir(__dirname+"/templates/*.ejs", "page")
+    templater.default().templateDir(__dirname+"/templates/form-inputs/*.ejs")
   }
 }
 
